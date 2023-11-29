b1aa930a89b6ec9ca63b31ef4eb3a1f5546169dd
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-11-29 11:26:23 -0500

    feat: logger

--- src/app.js
+++ src/app.js
@@ -9,7 +9,13 @@
 
 // console.log(person)
 
-const { getPokemonById } = require("./js-foundation/06-async")
+// const { getPokemonById } = require("./js-foundation/06-async")
 
-getPokemonById(3)
-    .then(pokemon => console.log({ pokemon }))
+// getPokemonById(3)
+// .then(pokemon => console.log({ pokemon }))
+
+const { buildLogger } = require("./plugins")
+
+const logger = buildLogger('app.js')
+logger.log('Hello world')
+logger.error('error')
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,11 @@
 const { getAge }  = require('./get-age.plugin')
 const { getUUID } = require('./get-id.plugin')
 const { httpClientPlugin } = require('./http-client.plugin')
+const buildLogger = require('./logger.plugin')
 
 module.exports = {
     httpClientPlugin,
     getAge,
     getUUID,
+    buildLogger,
 }
